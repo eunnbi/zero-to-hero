0cbc21f492345180ae17aef429cc83d866893c71
Eunbi Kang <jenabill@naver.com>
2023-12-03 21:37:58 +0900

    chore: remove character

--- todo_list_server/requests/main.py
+++ todo_list_server/requests/main.py
@@ -13,7 +13,7 @@ class Todo:
 def add_todos():
     headers = {"Content-Type": "application/json"}
     for i in range(1000):
-        body = Todo(title=f"test ${i}", content=f"add todo test {i}")
+        body = Todo(title=f"test {i}", content=f"add todo test {i}")
         try:
             response = requests.post(url=f"{BASE_URL}/todos", headers=headers, data=json.dumps(body.__dict__))
             data = response.json()
